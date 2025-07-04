73c002d1a7344fceede1b154fa98dc912c033eb8
Suvakanta Rout <suvakantarout2020@gmail.com>
2025-07-04 23:09:06 +0530

    some new style added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Navbar.css';
-import logo from '../assets/image-1.svg'
-
+import logo from '../assets/image-1.svg';
 
 const Navbar = () => {
+  const [darkMode, setDarkMode] = useState(true); // default is dark
+
+  useEffect(() => {
+    document.body.classList.toggle('dark-mode', darkMode);
+    document.body.style.backgroundColor = darkMode ? 'black' : 'white';
+    document.body.style.color = darkMode ? 'white' : 'black';
+  }, [darkMode]);
+
+  const toggleTheme = () => {
+    setDarkMode((prevMode) => !prevMode);
+  };
+
   return (
     <div className="Navbox">
       <img id="logo" src={logo} alt="Paste App Logo" />
-      <i class="ri-search-2-line" title="search icon"></i>
+      <i
+        className={darkMode ? 'ri-sun-fill' : 'ri-moon-fill'}
+        title="Toggle Theme"
+        onClick={toggleTheme}
+        style={{ cursor: 'pointer' }}
+      ></i>
     </div>
   );
 };
